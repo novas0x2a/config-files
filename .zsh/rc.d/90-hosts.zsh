732b29abe751d7184c9f93cafe19e4690c1341ae
Mike Lundy <mike@fluffypenguin.org>
2009-05-01 11:37:52 -0700

    firewire stuff for boink

--- .zsh/rc.d/90-hosts.zsh
+++ .zsh/rc.d/90-hosts.zsh
@@ -1,5 +1,7 @@
 case $(hostname --fqdn) in
     *arc.nasa.gov)
         IRGPKG=/irg/packages/${(L)$(echo $(uname -m)_$(uname -s)_gcc$(gcc -dumpversion | cut -f-2 -d .))}
+        path=($HOME/local/$(mytuple)/bin $path)
+        ldpath=($HOME/local/$(mytuple)/lib $ldpath)
         ;;
 esac
