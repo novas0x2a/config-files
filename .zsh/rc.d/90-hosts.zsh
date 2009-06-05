5fa97b2ca5f135c78bee5607b8396e6641ae6af2
Mike Lundy <mike@fluffypenguin.org>
2009-06-04 22:14:18 -0700

    roversw fixes

--- .zsh/rc.d/90-hosts.zsh
+++ .zsh/rc.d/90-hosts.zsh
@@ -3,5 +3,9 @@ case $(hostname --fqdn) in
         IRGPKG=/irg/packages/${(L)$(echo $(uname -m)_$(uname -s)_gcc$(gcc -dumpversion | cut -f-2 -d .))}
         path=($HOME/local/$(mytuple)/bin $path)
         ldpath=($HOME/local/$(mytuple)/lib $ldpath)
+        export HOME2=/usr/local/irg$HOME
+        if [[ -d "$HOME2" ]]; then
+            export CCACHE_DIR="$HOME2/.ccache"
+        fi
         ;;
 esac
