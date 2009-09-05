21f781f335300a53722a037a4087969e35f43213
Mike Lundy <mike@fluffypenguin.org>
2009-09-05 16:35:04 -0700

    zsh
    add virtualenv support

--- .zsh/rc.d/50-paths.zsh
+++ .zsh/rc.d/50-paths.zsh
@@ -3,6 +3,7 @@ for i in 2.4 2.5 2.6; do
         python_path=($HOME/local/lib/python${i}/site-packages $python_path)
     fi
 done
+
 python_path=($HOME/local/python $python_path)
 perl_path=($HOME/local/lib/perl5/site_perl $HOME/local/lib/perl5 $perl_path)
 
@@ -15,3 +16,8 @@ pkg_path=($HOME/local/lib/pkgconfig $pkg_path)
 export GEM_HOME=$HOME/.gems
 gem_path=($HOME/.gems /usr/lib/ruby /usr/lib/ruby/gems $gem_path)
 
+# Grab my usual virtualenv stuff
+if [[ -d  $HOME/local/python/bin ]]; then
+    export VIRTUAL_ENV="$HOME/local/python"
+    path=($VIRTUAL_ENV/bin $path)
+fi
