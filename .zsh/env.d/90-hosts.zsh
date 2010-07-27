f4544cadb059448884137b0e135eee07332dcdf8
Mike Lundy <mike@fluffypenguin.org>
2010-07-27 13:05:57 -0700

    add roversw release dir to path

--- .zsh/env.d/90-hosts.zsh
+++ .zsh/env.d/90-hosts.zsh
@@ -1,9 +1,9 @@
 # This code makes it fail gracefully on bsd hostname
 case $(hostname --fqdn 2>/dev/null || hostname) in
-    *ndc.nasa.gov)
+    *.ndc.nasa.gov)
         IRGARCH=${(L)$(echo $(uname -m)_$(uname -s)_gcc$(gcc -dumpversion | cut -f-2 -d .))}
         IRGPKG=/irg/packages/$IRGARCH
-        path=($HOME/local/$IRGARCH/bin $path)
+        path=($HOME/local/$IRGARCH/bin /usr/local/irg/releases/roversw/$IRGARCH/bin $path)
         ldpath=($HOME/local/$IRGARCH/lib $ldpath)
         pkg_path=($HOME/local/$IRGARCH/lib/pkgconfig $pkg_path)
         export HOME2=/usr/local/irg$HOME
