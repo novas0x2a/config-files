7f505ff03ced2b5d6681341082ff393d839cdd37
Mike Lundy <mike@fluffypenguin.org>
2011-09-01 15:11:38 -0700

    add some color

--- .zsh/rc.d/01-interactive.zsh
+++ .zsh/rc.d/01-interactive.zsh
@@ -1,6 +1,4 @@
 
-
-
 #case $TERM in xterm*|rxvt*)
 #    precmd () {
 #        print -Pn "\e]0;%n@%m: %~\a"
@@ -45,6 +43,10 @@ function preexec() {
   title "$cmd"
 }
 
+autoload colors zsh/terminfo
+if [[ "$terminfo[colors]" -ge 8 ]]; then
+  colors
+fi
 
 export PROMPT=$'%{\e[1;%(#|31|32)m%}%n@%m:%5~>%{\e[0m%} '
 export RPROMPT=$'%(?..[%{\e[1;31m%}%?%{\e[0m%}])$(__git_ps1)'
@@ -76,4 +78,5 @@ function title() {
       ;;
   esac
   setopt LOCAL_OPTIONS
+  return 0
 }
