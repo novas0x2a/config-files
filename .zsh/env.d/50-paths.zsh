8f3373ec264c725999770625d5c6b847fbea8c1b
Mike Lundy <mike@fluffypenguin.org>
2016-10-20 17:15:33 -0700

    meow

--- .zsh/env.d/50-paths.zsh
+++ .zsh/env.d/50-paths.zsh
@@ -1,14 +1,9 @@
 local PROJECT_DIR=~/Projects
 
-#if [[ -d $PROJECT_DIR/VisionWorkbench/scripts/python ]]; then
-#    python_path=($PROJECT_DIR/VisionWorkbench/scripts/python $python_path)
-#fi
-
 perl_path=($HOME/local/lib/perl5/site_perl $HOME/local/lib/perl5 $perl_path)
 
 fpath=($HOME/.zsh/functions $fpath)
 cdpath+=$PROJECT_DIR
-#path=(~/bin ~/.local/bin ~/local/noarch/bin ~/local/bin /opt/local/bin $path)
 path=(~/bin ~/.local/bin ~/local/bin /opt/local/bin $path)
 
 if which gem &>/dev/null; then
